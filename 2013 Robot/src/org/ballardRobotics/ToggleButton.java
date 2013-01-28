eb6512a2f00ccac26d7592b1575e20f1855febc2
lemonzap <adrian.macleod.jr@gmail.com>
2013-01-27 16:14:49 -0800

    added Toggle Button class

---
+++ 2013 Robot/src/org/ballardRobotics/ToggleButton.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ballardRobotics;
+
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+/**
+ *
+ * @author Lemonzap
+ */
+public class ToggleButton{
+    
+    JoystickButton button;
+    public boolean isPressed = false;
+    private boolean isToggled = false;
+    
+    public ToggleButton(JoystickButton receivedButton){
+        this.button = receivedButton;
+    }
+    
+    public boolean isToggled(){
+        if(this.button.get() && !isPressed){
+            isPressed = true;
+            isToggled = !isToggled;
+        }
+        if(!this.button.get()){
+            isPressed = false;
+        }
+            return isToggled;
+    }
+    
+    
+        
+}
