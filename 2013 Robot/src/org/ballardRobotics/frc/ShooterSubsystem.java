36f98edc0000ae6575f7a0902e7106e5f2d0b8d1
viking1 <viking1@team2928.com>
2013-01-29 19:30:32 -0800

    added Shooter subsystem class

---
+++ 2013 Robot/src/org/ballardRobotics/frc/ShooterSubsystem.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ballardRobotics.frc;
+
+/**
+ *
+ * @author Team 2928
+ */
+public class ShooterSubsystem {
+    
+}
