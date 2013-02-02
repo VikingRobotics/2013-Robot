732a8f58af0573e1761320cf92c61cf11097e861
viking2 <viking2@team2928.com>
2013-02-02 12:11:09 -0800

    Added a Motor controller multiplexer

--- 2013 Robot/src/org/ballardRobotics/frc/SpeedControllerMultiplexer.java
+++ 2013 Robot/src/org/ballardRobotics/frc/SpeedControllerMultiplexer.java
@@ -11,29 +11,40 @@ import edu.wpi.first.wpilibj.SpeedController;
  * @author Team 2928
  */
 public class SpeedControllerMultiplexer implements SpeedController{
-    private SpeedController controllers[];
+    public SpeedController controllers[];
+    private double speed = 0;
     
     public SpeedControllerMultiplexer(SpeedController[] controllers){
         this.controllers = controllers;
     }
 
     public double get(){
+        return speed;
     }
 
-    public void set(double d, byte b){
+    public void set(double speed, byte syncGroup){
+        this.speed = speed;
+        for(int i = 0; i < controllers.length; i++){
+            controllers[i].set(speed, syncGroup);
+        }
     }
 
-    public void set(double d){
+    public void set(double speed){
+        this.speed = speed;
+        for(int i = 0; i < controllers.length; i++){
+            controllers[i].set(speed);
+        }
     }
 
     public void disable(){
+        for(int i = 0; i < controllers.length; i++){
+            controllers[i].disable();
+        }
     }
 
-    public void pidWrite(double d){
+    public void pidWrite(double output){
+        for(int i = 0; i < controllers.length; i++){
+            controllers[i].pidWrite(output);
+        }
     }
-    
-    
-    
-    
-    
 }
